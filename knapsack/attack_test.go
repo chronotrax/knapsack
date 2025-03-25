ef3909440dc52d074fe5ec41c18a46fffcce7ae9
chronotrax <55108934+chronotrax@users.noreply.github.com>
2025-03-24 22:28:11 -0400

    Test.

--- knapsack/attack_test.go
+++ knapsack/attack_test.go
@@ -1,7 +1,6 @@
 package knapsack
 
 import (
-	"github.com/chronotrax/knapsack/matrix"
 	"math/big"
 	"reflect"
 	"testing"
@@ -9,31 +8,33 @@ import (
 
 func Test_gs(t *testing.T) {
 	type args struct {
-		b matrix.Matrix
+		n int
+		b []*big.Float
 	}
 	tests := []struct {
 		name string
 		args args
-		want matrix.Matrix
+		want []*big.Float
 	}{
 		{
 			name: "1",
 			args: args{
-				b: matrix.NewMatrixFull(3, 3,
-					matrix.Vector{new(big.Rat).SetInt64(1), new(big.Rat).SetInt64(1), new(big.Rat).SetInt64(1),
-						new(big.Rat).SetInt64(-1), new(big.Rat).SetInt64(0), new(big.Rat).SetInt64(1),
-						new(big.Rat).SetInt64(1), new(big.Rat).SetInt64(1), new(big.Rat).SetInt64(2)}),
+				n: 3,
+				b: []*big.Float{big.NewFloat(1), big.NewFloat(1), big.NewFloat(1),
+					big.NewFloat(-1), big.NewFloat(0), big.NewFloat(1),
+					big.NewFloat(1), big.NewFloat(1), big.NewFloat(2)},
 			},
-			want: matrix.NewMatrixFull(3, 3,
-				matrix.Vector{new(big.Rat).SetInt64(1), new(big.Rat).SetFrac64(1, 3), new(big.Rat).SetFrac64(-1, 2),
-					new(big.Rat).SetInt64(-1), new(big.Rat).SetFrac64(2, 3), new(big.Rat).SetInt64(0),
-					new(big.Rat).SetInt64(1), new(big.Rat).SetFrac64(1, 3), new(big.Rat).SetFrac64(1, 2)}),
+			want: []*big.Float{big.NewFloat(1), big.NewFloat(1 / 3), big.NewFloat(-1 / 2),
+				big.NewFloat(-1), big.NewFloat(2 / 3), big.NewFloat(0),
+				big.NewFloat(1), big.NewFloat(1 / 3), big.NewFloat(1 / 2)},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got, _ := gs(tt.args.b); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("gs() = %v, want %v", got, tt.want)
+			mu := emptySlice(tt.args.n * tt.args.n)
+			B := emptySlice(tt.args.n)
+			if gs(tt.args.b, mu, B, tt.args.n); !reflect.DeepEqual(tt.args.b, tt.want) {
+				t.Errorf("gs() = %v, want %v", tt.args.b, tt.want)
 			}
 		})
 	}
@@ -41,33 +42,31 @@ func Test_gs(t *testing.T) {
 
 func Test_lll(t *testing.T) {
 	type args struct {
-		b             matrix.Matrix
-		delta         *big.Rat
-		maxIterations int
+		n     int
+		b     []*big.Float
+		delta *big.Float
 	}
 	tests := []struct {
 		name string
 		args args
-		want matrix.Matrix
+		want []*big.Float
 	}{
 		{
 			name: "1",
 			args: args{
-				b: matrix.NewMatrixFull(2, 2,
-					matrix.Vector{new(big.Rat).SetInt64(47), new(big.Rat).SetInt64(95),
-						new(big.Rat).SetInt64(215), new(big.Rat).SetInt64(460)}),
-				delta:         new(big.Rat).SetFrac64(3, 4),
-				maxIterations: 1000,
+				n: 2,
+				b: []*big.Float{big.NewFloat(47), big.NewFloat(95),
+					big.NewFloat(215), big.NewFloat(460)},
+				delta: big.NewFloat(3 / 4),
 			},
-			want: matrix.NewMatrixFull(2, 2,
-				matrix.Vector{new(big.Rat).SetInt64(1), new(big.Rat).SetInt64(40),
-					new(big.Rat).SetInt64(30), new(big.Rat).SetInt64(5)}),
+			want: []*big.Float{big.NewFloat(1), big.NewFloat(40),
+				big.NewFloat(30), big.NewFloat(5)},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := lll(tt.args.b, tt.args.delta, tt.args.maxIterations); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("lll() = %v, want %v", got, tt.want)
+			if lll(tt.args.b, tt.args.n, big.NewFloat(1/2), tt.args.delta); !reflect.DeepEqual(tt.args.b, tt.want) {
+				t.Errorf("lll() = %v, want %v", tt.args.b, tt.want)
 			}
 		})
 	}
