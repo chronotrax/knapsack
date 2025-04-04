995f752e0a53c939acc02a1a078f84891654ebd2
chronotrax <55108934+chronotrax@users.noreply.github.com>
2025-04-04 13:22:50 -0400

    Float64 experiment.

--- knapsack/attack.go
+++ knapsack/attack.go
@@ -2,13 +2,14 @@ package knapsack
 
 import (
 	"fmt"
+	"math"
 	"math/big"
 	"slices"
 )
 
 // gs is the Gram–Schmidt algorithm.
-func gs(b, mu, B []*big.Float, n int) {
-	bStar := emptySlice(n * n)
+func gs(b, mu, B []float64, n int) {
+	bStar := make([]float64, n*n)
 
 	for i := 0; i < n; i++ {
 		// b*_i = b_i
@@ -18,39 +19,39 @@ func gs(b, mu, B []*big.Float, n int) {
 
 		// Initialize a term to keep track of the summation term
 		// in the gram schmidt algorithm
-		summand := emptySlice(n)
+		summand := make([]float64, n)
 
 		// Calculate mu_ij and summand value
 		for j := 0; j < i; j++ {
 			// mu_ij = <b_i, b*_j>/<b*_j, b*_j>
-			numeratorMu := new(big.Float)
+			var numeratorMu float64
 			for k := 0; k < n; k++ {
-				numeratorMu = new(big.Float).Add(numeratorMu, new(big.Float).Mul(b[k*n+i], bStar[k*n+j]))
+				numeratorMu = numeratorMu + b[k*n+i]*bStar[k*n+j]
 			}
-			mu[i*n+j] = new(big.Float).Quo(numeratorMu, B[j])
+			mu[i*n+j] = numeratorMu / B[j]
 
 			for k := 0; k < n; k++ {
-				summand[k] = new(big.Float).Add(summand[k], new(big.Float).Mul(mu[i*n+j], bStar[k*n+j]))
+				summand[k] = summand[k] + mu[i*n+j]*bStar[k*n+j]
 			}
 		}
 
 		// Calculate new b_star values and B values
 		for j := 0; j < n; j++ {
-			bStar[j*n+i] = new(big.Float).Sub(bStar[j*n+i], summand[j])
+			bStar[j*n+i] = bStar[j*n+i] - summand[j]
 		}
 
-		dot := new(big.Float)
+		var dot float64
 		for j := 0; j < n; j++ {
-			dot = new(big.Float).Add(dot, new(big.Float).Mul(bStar[j*n+i], bStar[j*n+i]))
+			dot = dot + bStar[j*n+i]*bStar[j*n+i]
 		}
 		B[i] = dot
 	}
 }
 
 // lll is the Lenstra–Lenstra–Lovász lattice basis reduction algorithm
-func lll(b []*big.Float, n int, lc, uc *big.Float) {
-	mu := emptySlice(n * n)
-	B := emptySlice(n)
+func lll(b []float64, n int, lc, uc float64) {
+	mu := make([]float64, n*n)
+	B := make([]float64, n)
 
 	gs(b, mu, B, n)
 
@@ -58,28 +59,28 @@ func lll(b []*big.Float, n int, lc, uc *big.Float) {
 	for k < n {
 		// Step 1 of LLL, achieve m_(k,k-1) <= lc
 		// This is condition (1.18) from LLL
-		if new(big.Float).Abs(mu[k*n+k-1]).Cmp(lc) > 0 {
-			r := new(big.Float).SetPrec(53).Set(mu[k*n+k-1])
+		if mu[k*n+k-1] > lc {
+			r := math.Round(mu[k*n+k-1])
 
 			// replace b_k by b_k - r*b_(k-1)
 			for i := 0; i < n; i++ {
-				b[k+i*n] = new(big.Float).Sub(b[k+i*n], new(big.Float).Mul(r, b[k-1+i*n]))
+				b[k+i*n] = b[k+i*n] - r*b[k-1+i*n]
 			}
 
 			// Change the mu's accordingly
 			for i := 0; i < k-1; i++ {
-				mu[k*n+i] = new(big.Float).Sub(mu[k*n+i], new(big.Float).Mul(r, mu[(k-1)*n+i]))
+				mu[k*n+i] = mu[k*n+i] - r*mu[(k-1)*n+i]
 			}
-			mu[k*n+k-1] = new(big.Float).Sub(mu[k*n+k-1], r)
+			mu[k*n+k-1] = mu[k*n+k-1] - r
 		}
 
 		// This completes step one of the algorithm
 		// Step 2
 		// Case 1: B_k + (mu(k,k-1)^2)B_(k-1) < uc*B_(k-1)
-		left := new(big.Float).Add(B[k], new(big.Float).Mul(new(big.Float).Mul(mu[k*n+k-1], mu[k*n+k-1]), B[k-1]))
-		right := new(big.Float).Mul(uc, B[k-1])
+		left := B[k] + mu[k*n+k-1]*mu[k*n+k-1]*B[k-1]
+		right := uc * B[k-1]
 
-		if left.Cmp(right) < 0 && k > 0 {
+		if left < right && k > 0 {
 			// First swap b_k and b_(k-1)
 			for i := 0; i < n; i++ {
 				temp := b[i*n+k]
@@ -90,21 +91,20 @@ func lll(b []*big.Float, n int, lc, uc *big.Float) {
 			// We need to save three values for the rest of Step 2 case 1, these
 			// are B_k, B_k + mu_(k,k-1)*B[k-1], and mu_(k,k-1)
 			BTemp := B[k]
-			C := new(big.Float).Add(B[k], new(big.Float).Mul(new(big.Float).Mul(mu[k*n+k-1], mu[k*n+k-1]), B[k-1]))
+			C := B[k] + mu[k*n+k-1]*mu[k*n+k-1]*B[k-1]
 			muTemp := mu[k*n+k-1]
 
 			// Now we continue with the algorithm, first adjust B
-			mu[k*n+k-1] = new(big.Float).Quo(new(big.Float).Mul(muTemp, B[k-1]), C)
-			B[k] = new(big.Float).Quo(new(big.Float).Mul(B[k-1], B[k]), C)
+			mu[k*n+k-1] = muTemp * B[k-1] / C
+			B[k] = B[k-1] * B[k] / C
 			B[k-1] = C
 
 			// All other B values stay the same
 			// Next we adjust mu
 			for i := k + 1; i < n; i++ {
 				temp := mu[i*n+k-1]
-				mu[i*n+k-1] = new(big.Float).Add(new(big.Float).Mul(mu[i*n+k-1], mu[k*n+k-1]),
-					new(big.Float).Quo(new(big.Float).Mul(mu[i*n+k], BTemp), C))
-				mu[i*n+k] = new(big.Float).Sub(temp, new(big.Float).Mul(mu[i*n+k], muTemp))
+				mu[i*n+k-1] = mu[i*n+k-1]*mu[k*n+k-1] + mu[i*n+k]*BTemp/C
+				mu[i*n+k] = temp - mu[i*n+k]*muTemp
 			}
 
 			for i := 0; i < k-1; i++ {
@@ -121,19 +121,19 @@ func lll(b []*big.Float, n int, lc, uc *big.Float) {
 			l := k
 			for l > 0 {
 				l = l - 1
-				if new(big.Float).Abs(mu[k*n+l]).Cmp(lc) > 0 {
-					r := new(big.Float).SetPrec(53).Set(mu[k*n+l])
+				if math.Abs(mu[k*n+l]) > lc {
+					r := math.Round(mu[k*n+l])
 
 					// b_k = b_k - r*b_l
 					for i := 0; i < n; i++ {
-						b[i*n+k] = new(big.Float).Sub(b[i*n+k], new(big.Float).Mul(r, b[i*n+l]))
+						b[i*n+k] = b[i*n+k] - r*b[i*n+l]
 					}
 
 					for i := 0; i < l; i++ {
-						mu[k*n+i] = new(big.Float).Sub(mu[k*n+i], new(big.Float).Mul(r, mu[l*n+i]))
+						mu[k*n+i] = mu[k*n+i] - r*mu[l*n+i]
 					}
 
-					mu[k*n+l] = new(big.Float).Sub(mu[k*n+l], r)
+					mu[k*n+l] = mu[k*n+l] - r
 					// The other mu are unchanged
 					l = k
 				}
@@ -145,18 +145,18 @@ func lll(b []*big.Float, n int, lc, uc *big.Float) {
 
 // checkColumn checks if the c column of Matrix m is in the correct form:
 // 0 to n-2 are all 0's or 1's, and n-1 is a 0.
-func checkColumn(c []*big.Float, col, n int) bool {
+func checkColumn(c []float64, col, n int) bool {
 	// 0 to n-2 must be a 0 or 1
 	for i := 0; i < n-1; i++ {
-		t := new(big.Float).Set(c[col*n+i])
+		t := c[col*n+i]
 		// if t != 0 and t != 1
-		if t.Cmp(big.NewFloat(0)) != 0 && t.Cmp(big.NewFloat(1)) != 0 {
+		if t != 0 && t != 1 {
 			return false
 		}
 	}
 
 	// the last value must be == 0
-	if c[n*(n-1)+col].Cmp(big.NewFloat(0)) != 0 {
+	if c[n*(n-1)+col] != 0 {
 		return false
 	}
 
@@ -177,25 +177,25 @@ func Attack(blockSize int, cipher Ciphertext, public PublicKey, expected []byte)
 
 	// n is 1 larger than the original block n
 	n := len(public) + 1
-	m := emptySlice(n * n)
+	m := make([]float64, n*n)
 
 	// make 0 to n-1 an identity matrix
 	for i := 0; i < n-1; i++ {
-		m[i*n+i] = big.NewFloat(1)
+		m[i*n+i] = 1
 	}
 
 	// make bottom row (0 to n-1) the public key
 	for i := 0; i < n-1; i++ {
-		m[n*(n-1)+i] = new(big.Float).SetInt(public[i])
+		m[n*(n-1)+i] = float64(public[i].Int64())
 	}
 
 	// make bottom right corner -1*cipher
-	m[n*n-1] = new(big.Float).Mul(new(big.Float).SetInt(cipher[0]), big.NewFloat(-1))
+	m[n*n-1] = float64(cipher[0].Int64() * -1)
 
 	fmt.Println("initial matrix:")
 	printM(m, n)
 
-	lll(m, n, big.NewFloat(0.5), big.NewFloat(0.75))
+	lll(m, n, 0.5, 0.75)
 
 	fmt.Println("reduced matrix")
 	printM(m, n)
@@ -208,8 +208,7 @@ func Attack(blockSize int, cipher Ciphertext, public PublicKey, expected []byte)
 
 			plain := make(Plaintext, len(public))
 			for j := 0; j < len(public); j++ {
-				f, _ := m[i+j*n].Int(new(big.Int))
-				plain[j] = new(big.Int).Set(f)
+				plain[j] = new(big.Int).SetInt64(int64(m[i+j*n]))
 			}
 
 			data := k.FromPlaintext(plain)
@@ -226,18 +225,10 @@ func Attack(blockSize int, cipher Ciphertext, public PublicKey, expected []byte)
 	}
 }
 
-func emptySlice(n int) []*big.Float {
-	m := make([]*big.Float, n*n)
-	for i := 0; i < n; i++ {
-		m[i] = new(big.Float)
-	}
-	return m
-}
-
-func printM(m []*big.Float, n int) {
+func printM(m []float64, n int) {
 	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
-			fmt.Printf("%v ", m[i*n+j].String())
+			fmt.Printf("%v ", m[i*n+j])
 		}
 		fmt.Println("")
 	}
--- knapsack/attack_test.go
+++ knapsack/attack_test.go
@@ -1,7 +1,6 @@
 package knapsack
 
 import (
-	"math/big"
 	"reflect"
 	"testing"
 )
@@ -9,30 +8,30 @@ import (
 func Test_gs(t *testing.T) {
 	type args struct {
 		n int
-		b []*big.Float
+		b []float64
 	}
 	tests := []struct {
 		name string
 		args args
-		want []*big.Float
+		want []float64
 	}{
 		{
 			name: "1",
 			args: args{
 				n: 3,
-				b: []*big.Float{big.NewFloat(1), big.NewFloat(1), big.NewFloat(1),
-					big.NewFloat(-1), big.NewFloat(0), big.NewFloat(1),
-					big.NewFloat(1), big.NewFloat(1), big.NewFloat(2)},
+				b: []float64{1, 1, 1,
+					-1, 0, 1,
+					1, 1, 2},
 			},
-			want: []*big.Float{big.NewFloat(1), big.NewFloat(1 / 3), big.NewFloat(-1 / 2),
-				big.NewFloat(-1), big.NewFloat(2 / 3), big.NewFloat(0),
-				big.NewFloat(1), big.NewFloat(1 / 3), big.NewFloat(1 / 2)},
+			want: []float64{1, 1 / 3, -1 / 2,
+				-1, 2 / 3, 0,
+				1, 1 / 3, 1 / 2},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			mu := emptySlice(tt.args.n * tt.args.n)
-			B := emptySlice(tt.args.n)
+			mu := make([]float64, tt.args.n*tt.args.n)
+			B := make([]float64, tt.args.n)
 			if gs(tt.args.b, mu, B, tt.args.n); !reflect.DeepEqual(tt.args.b, tt.want) {
 				t.Errorf("gs() = %v, want %v", tt.args.b, tt.want)
 			}
@@ -43,29 +42,29 @@ func Test_gs(t *testing.T) {
 func Test_lll(t *testing.T) {
 	type args struct {
 		n     int
-		b     []*big.Float
-		delta *big.Float
+		b     []float64
+		delta float64
 	}
 	tests := []struct {
 		name string
 		args args
-		want []*big.Float
+		want []float64
 	}{
 		{
 			name: "1",
 			args: args{
 				n: 2,
-				b: []*big.Float{big.NewFloat(47), big.NewFloat(95),
-					big.NewFloat(215), big.NewFloat(460)},
-				delta: big.NewFloat(3 / 4),
+				b: []float64{47, 94,
+					215, 460},
+				delta: 3 / 4,
 			},
-			want: []*big.Float{big.NewFloat(1), big.NewFloat(40),
-				big.NewFloat(30), big.NewFloat(5)},
+			want: []float64{1, 40,
+				30, 5},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if lll(tt.args.b, tt.args.n, big.NewFloat(1/2), tt.args.delta); !reflect.DeepEqual(tt.args.b, tt.want) {
+			if lll(tt.args.b, tt.args.n, 1/2, tt.args.delta); !reflect.DeepEqual(tt.args.b, tt.want) {
 				t.Errorf("lll() = %v, want %v", tt.args.b, tt.want)
 			}
 		})
